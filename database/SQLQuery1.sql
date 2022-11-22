74c2dea52186857672f97e84520f2bc743605664
PedroAugustoB <99665373+PedroAugustoB@users.noreply.github.com>
2022-11-22 01:37:40 -0300

    Add files via upload

---
+++ database/SQLQuery1.sql
@@ -0,0 +1,11 @@
+use healthful
+
+select * from usuario;
+
+insert into usuario (nomeUser, emailUser,senhaUser,nivelUser) values('pedro', 'pedro@oi','00990',2);
+
+-----------------
+
+select * from postagem; 
+
+insert into postagem (tituloPost, textPost) values('Eu sou o melhor', 'oi vc ai que ta lendo essa mensagem sim vc mesmo que agora ta se perguntando se é com vc é vc mesmo sim com vc');
