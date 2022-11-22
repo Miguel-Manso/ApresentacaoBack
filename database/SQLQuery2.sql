73df93682755ef7b3a752907179697c777f43f70
PedroAugustoB <99665373+PedroAugustoB@users.noreply.github.com>
2022-11-22 10:31:58 -0300

    Add files via upload

---
+++ database/SQLQuery2.sql
@@ -0,0 +1,9 @@
+use healthful
+
+select * from postagem;
+
+insert into usuario (nomeUser, emailUser,senhaUser,nivelUser) values('miguel', 'migel@oi','990999',1);
+
+
+
+update usuario set nomeUser = 'verde', emailUser = 'uuu@uuu', senhaUser = '21212', nivelUser = 8 where idUser = 1;
